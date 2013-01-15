5ee8f1f92aecb81a6a68c0d5cbc72e5a8def2b92
DKOSP <dkofficials@gmail.com>
2013-01-15 06:28:30 -0500

    Update config/common_full_tablet_wifionly.mk

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,6 +1,6 @@
 # Inherit common stuff
-$(call inherit-product, vendor/ev/config/common.mk)
-$(call inherit-product, vendor/ev/config/common_full.mk)
+$(call inherit-product, vendor/dkosp/config/common.mk)
+$(call inherit-product, vendor/dkosp/config/common_full.mk)
 
 # More tablet-specific items
 
