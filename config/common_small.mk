ce1eaac154b9c14ffe6d50cf88ca9f2771297b08
DKOSP <dkofficials@gmail.com>
2013-01-15 06:29:03 -0500

    Update config/common_small.mk

--- config/common_small.mk
+++ config/common_small.mk
@@ -1,4 +1,4 @@
 #Config used for small targets, with telephony or without.
 
 # English Dictionary
-PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/dictionaries/english
+PRODUCT_PACKAGE_OVERLAYS += vendor/dkosp/overlay/dictionaries/english
