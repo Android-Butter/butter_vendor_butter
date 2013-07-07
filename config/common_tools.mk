54976dd7bf30436feaec45c36c2173eedfbd8544
PURE AOSP! <androidbutter@gmail.com>
2013-07-07 06:02:55 -0400

    Update common_tools.mk

--- config/common_tools.mk
+++ config/common_tools.mk
@@ -26,7 +26,6 @@ PRODUCT_PACKAGES += \
 # Utilize init.d scripts
 PRODUCT_COPY_FILES += \
     vendor/butter/prebuilt/common/bin/sysinit:system/bin/sysinit \
-    vendor/butter/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/butter/prebuilt/common/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
     vendor/butter/prebuilt/common/etc/init.d/05mountext:system/etc/init.d/05mountext \
     vendor/butter/prebuilt/common/etc/init.d/06handleswap:system/etc/init.d/06handleswap \
